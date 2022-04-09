478ddd3574586b299545f9693f4e56830af449d0
Nam Nguyen <nnguy185@calpoly.edu>
2022-04-09 12:33:59 -0700

    Update va.py

--- va.py
+++ va.py
@@ -2,15 +2,19 @@ import pyttsx3  # converts text to speech
 import datetime  # required to resolve any query regarding date and time
 import speech_recognition as sr  # required to return a string output by taking microphone input from the user
 import wikipedia  # required to resolve any query regarding wikipedia
-import webbrowser  # required to open the prompted application in web browser
+import webbrowser  # required to open the prompted link in web browser
 import requests
 from GoogleNews import GoogleNews
 
 # Global variables
-engine = pyttsx3.init()
-googlenews = GoogleNews()
+# Global variables
+engine = pyttsx3.init() # Initilaize text-to-speech engine
+googlenews = GoogleNews() # Initlize Gooogle News Engine 
 
-link = "https://www.youtube.com/watch?v=8pm_KoguqPM"
+# Global CONSTANTS
+YOUTUBE_LINK  = "https://www.youtube.com/watch?v=8pm_KoguqPM" # For opening Youtube Link
+SLO_CORD = ["35.2913508", "-120.6618469"] # [lat, lon] of SLO, for Weather API
+WEATHER_API_KEY = None # Weather API Key
 
 
 def get_weather():

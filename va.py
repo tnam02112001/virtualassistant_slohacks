f0feccc7b2ecb38064d46c36f50f2a3cdda5f614
Nam Nguyen <nnguy185@calpoly.edu>
2022-04-09 13:53:51 -0700

    Update va.py

--- va.py
+++ va.py
@@ -14,7 +14,7 @@ googlenews = GoogleNews() # Initlize Gooogle News Engine
 # Global CONSTANTS
 YOUTUBE_LINK  = "https://www.youtube.com/watch?v=8pm_KoguqPM" # For opening Youtube Link
 SLO_CORD = ["35.2913508", "-120.6618469"] # [lat, lon] of SLO, for Weather API
-WEATHER_API_KEY = None # Weather API Key
+WEATHER_API_KEY = "d1032fab5119ba55d9fe5f5cab2967f0" # Weather API Key
 
    
 if __name__ == "__main__":  # execution control
